7bf5e239a78430c4a07ec344e6ca310086b49f35
Marios Konidaris <fanatika.f1@gmail.com>
2019-04-25 00:48:18 +0300

    Made the Game class but haven't finished it yet. There are things to be done like taking the second value from the map and to check if a player has won the round.

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -6,12 +6,9 @@ Player::Player() {
   std::cout << "Hello player what's your name ?" << std::endl;
   std::cout << "Name: ";
   std::getline(std::cin, name);
-  std::cout << "So, " << name << ", what's your favourite color ?" << std::endl;
+  std::cout << "So, " << name << ", what color do you prefer? Blue or red?" << std::endl;
   std::cout << "Color: ";
   std::cin >> favColor;
-}
-
-void Player::showInfo() {
-  std::cout << name << std::endl;
-  std::cout << favColor << std::endl;
+  char f = favColor[0];
+  col.insert(std::pair<std::string, char>(favColor, f));
 }
--- Player/Player.h
+++ Player/Player.h
@@ -6,8 +6,10 @@ class Player {
 private:
   std::string name;
   std::string favColor;
+  std::map<std::string, char> col;
+
+  int score;
 
 public:
   Player();
-  void showInfo();
 };
